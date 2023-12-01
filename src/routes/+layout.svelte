a69f2ca5b8db40eec3b33e6a39e7a6f22eed8fa2
Paul Adams <ghusiyy@gmail.com>
2023-11-30 23:40:21 -0800

    add layout component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<svelte:head>
+  <title>Heritage Oven Chronicle</title>
+  <meta
+    name="description"
+    content="document and share a heritage of baking recipes and culinary traditions"
+  />
+</svelte:head>
+
+<header>
+  <h2>Heritage Oven Chronicle</h2>
+  <nav>
+    <ul>
+      <li>Home</li>
+    </ul>
+  </nav>
+</header>
+<main>
+  <slot />
+</main>
+<footer>footer</footer>
